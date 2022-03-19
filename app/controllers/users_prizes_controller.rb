6e2148e2fbf572f9b7351e8800ec7e276157809a
mciepluch <maciej.ciepluch@htdevelopers.com>
2022-03-19 07:48:42 +0100

    Add pagination to users prizes

--- app/controllers/users_prizes_controller.rb
+++ app/controllers/users_prizes_controller.rb
@@ -2,14 +2,17 @@ class UsersPrizesController < ApplicationController
   before_action :set_user_prize, only: %i[show destroy update]
 
   def index
-    render json: current_user.users_prizes, status: :ok
+    page = params[:page] || 1
+    current_records = current_user.users_prizes.page(page).per(15)
+    render json: { resources: current_records, total: current_records.total_pages, current_page: current_records.current_page }, status: :ok
   end
 
   def create
     new_prize = current_user.users_prizes.new(user_prize_params_create)
     current_user.points -= Prize.find(params[:user_prize][:prize_id]).price
-    if !current_user.points.positive?
+    unless current_user.points.positive?
       render status: :forbidden
+      return
     end
     if new_prize.save && current_user.save
       render status: :ok
