a40bc7427360ea47d0b5051deee2acdddbbcbe1e
mciepluch <maciej.ciepluch@htdevelopers.com>
2022-03-20 07:34:43 +0100

    Change order of history points

--- app/controllers/history_points_controller.rb
+++ app/controllers/history_points_controller.rb
@@ -1,7 +1,7 @@
 class HistoryPointsController < ApplicationController
   def index
     page = params[:page] || 1
-    current_records = current_user.history_points.page(page).per(15)
+    current_records = current_user.history_points.order("created_at DESC").page(page).per(15)
     render json: { resources: current_records, total: current_records.total_pages, current_page: current_records.current_page },
            status: :ok
   end
